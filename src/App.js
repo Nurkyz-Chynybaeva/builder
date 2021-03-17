3da86047c7d5bd97b2f1458e71e72a7ceb5f9626
Nurkyz-Chynybaeva <nurkyzchynybaeva29@gmail.com>
2021-03-17 15:22:20 +0600

    created files Nav Logo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import Layout from "./components/Layout/Layout";
+import Toolbar from "./components/Toolbar/Toolbar";
 
 
 function App() {
   return (
     <div className="App">
       <Layout>
+<Toolbar />
+
 Awesome builder by Nurkyz Chynybaeva 
       </Layout>
 
--- src/components/Toolbar/Toolbar.js
+++ src/components/Toolbar/Toolbar.js
@@ -3,7 +3,8 @@ import classes from "./Toolbar.module.css";
 const Toolbar = ({children}) => {
   return (
     <div className = {classes.Toolbar}>
-     {children}
+    <Logo/>
+    <Nav/>
     </div>
   )
 }
