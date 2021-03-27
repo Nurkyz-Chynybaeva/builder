58e8221fc53fda1316e8acea3a8fd2c9b4080f5f
Nurkyz-Chynybaeva <nurkyzchynybaeva29@gmail.com>
2021-03-27 15:12:26 +0600

    const decor control

--- src/components/DecorBuilder/DecorControls/DecorControl/DecorControl.js
+++ src/components/DecorBuilder/DecorControls/DecorControl/DecorControl.js
@@ -1,4 +1,14 @@
 import classes from "./DecorControl.module.css";                  
 import DecorControl from "./DecorControl/DecorControl";
 
+const DecorControl = ( {type , add , remove}) => {
+
+
+
+
+
+
+
+
+
 
