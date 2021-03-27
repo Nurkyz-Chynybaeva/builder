0085760ba90dccaaa75347b7ad2070325b61196b
Nurkyz-Chynybaeva <nurkyzchynybaeva29@gmail.com>
2021-03-27 14:57:38 +0600

    const DecorBuilder

--- src/components/DecorBuilder/DecorBuilder.js
+++ src/components/DecorBuilder/DecorBuilder.js
@@ -2,3 +2,13 @@ import classes from "./DecorBuilder.module.css";
 import DecorPreview from "./DecorPreview/DecorPreview";
 import DecorControls from "./DecorControls/DecorControls";
 import {useState} from "react";
+
+
+const DecorBuilder = () => {
+    
+    const [butterflies , setButterflies ] = useState({ 
+
+      
+
+    });
+  
