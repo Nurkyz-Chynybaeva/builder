cd45a632f1425898bbc0bf53baf7f95756677a6b
Nurkyz-Chynybaeva <nurkyzchynybaeva29@gmail.com>
2021-03-27 15:22:13 +0600

    remove butterly

--- src/components/DecorBuilder/DecorControls/DecorControls.js
+++ src/components/DecorBuilder/DecorControls/DecorControls.js
@@ -1,3 +1,15 @@
 import classes from "./DecorControls.module.css";
 import DecorControl from "./DecorControl/DecorControl";
 const DecorControls =({butterflies , addButterfly , removeButterfly}) =>{
+
+    removeButterfly}) =>{
+        let type = {butterflies}
+
+
+
+
+
+
+
+
+
