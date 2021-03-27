d5fe1933b0be58b9c2e8e95f6ef83d4b3eeb64b1
Nurkyz-Chynybaeva <nurkyzchynybaeva29@gmail.com>
2021-03-27 15:27:57 +0600

    added function add and remove

--- src/components/DecorBuilder/DecorControls/DecorControls.js
+++ src/components/DecorBuilder/DecorControls/DecorControls.js
@@ -1,14 +1,17 @@
 import classes from "./DecorControls.module.css";
 import DecorControl from "./DecorControl/DecorControl";
 const DecorControls =({butterflies , addButterfly , removeButterfly}) =>{
-
-    removeButterfly}) =>{
+      removeButterfly}) =>{
         let type = {butterflies}
         const results = []
         for (const butterfly in butterflies) {
             results.push(<DecorControl 
                 type = {butterfly}
-               
+                add={addButterfly}
+    remove = {removeButterfly}
+    
+    />)
+    }
 
 
 
