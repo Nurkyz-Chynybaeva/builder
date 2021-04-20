ecf1994f2366219ee0237547b403fde8ee57fe19
Nurkyz-Chynybaeva <nurkyzchynybaeva29@gmail.com>
2021-04-20 13:49:54 +0600

    fix path backdrop in drawer.js

--- src/components/Drawer/Drawer.js
+++ src/components/Drawer/Drawer.js
@@ -1,10 +1,6 @@
 import Logo from "../Logo/Logo";
 import Nav from "../Nav/Nav";
-<<<<<<< HEAD
 import Backdrop from "../UL/Backdrop/Backdrop";
-=======
-import Backdrop from "../Ul/Backdrop/Backdrop";
->>>>>>> 7f7ef59 (fix path backdrop in drawer.js)
 import classes from "./Drawer.module.css";
 
 const Drawer = ({ open, closeDrawer }) => {
