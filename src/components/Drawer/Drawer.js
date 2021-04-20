8ab429f9a035a6e20e78c00128f0807968670243
Nurkyz-Chynybaeva <nurkyzchynybaeva29@gmail.com>
2021-04-20 14:06:47 +0600

    Pull project

--- src/components/Drawer/Drawer.js
+++ src/components/Drawer/Drawer.js
@@ -1,6 +1,10 @@
 import Logo from "../Logo/Logo";
 import Nav from "../Nav/Nav";
+<<<<<<< HEAD
 import Backdrop from "../UL/Backdrop/Backdrop";
+=======
+import Backdrop from "../Ul/Backdrop/Backdrop";
+>>>>>>> 7f7ef59 (fix path backdrop in drawer.js)
 import classes from "./Drawer.module.css";
 
 const Drawer = ({ open, closeDrawer }) => {
