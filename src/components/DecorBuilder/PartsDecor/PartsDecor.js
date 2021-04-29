c038875605ab7696bf3f42a2c6ba417a58f58a7d
Nurkyz-Chynybaeva <nurkyzchynybaeva29@gmail.com>
2021-04-29 22:40:26 +0600

    background size cover

--- src/components/DecorBuilder/PartsDecor/PartsDecor.js
+++ src/components/DecorBuilder/PartsDecor/PartsDecor.js
@@ -14,10 +14,39 @@ const PartsDecor = ({ type, fixed }) => {
     
   }
 
+
+  function getPosition(butterflyWidth) {
+    const diameter = 380;
+    const radius = diameter / 2;
+    const butterflyradius = parseInt(butterflyWidth) / 2;
+
+    const butterflyTop = Math.round(Math.random() * diameter);
+    const butterflyLeft = Math.round(Math.random() * diameter);
+
+    const distance = Math.sqrt(
+      Math.pow(butterflyTop - radius, 2) + Math.pow(butterflyLeft - radius, 2)
+    ) + butterflyradius;
+
+    return distance < radius
+      ? {
+        top: butterflyTop - butterflyradius,
+        left: butterflyLeft - butterflyradius
+      }
+      : getPosition(butterflyWidth);
+  }
+
+  // Get random position for this ingredient.
+  if (!fixed) {
+    const position = getPosition(types[type].width);
+    types[type].top = position.top + "px";
+    types[type].left = position.left + "px";
+  }
+  // Get random rotation for this ingredient.
+
+
   return (
     <div className={classes.PartsDecor} style={types[type]}></div>
   );
-
 }
 
 
