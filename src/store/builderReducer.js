20f446e6fe362b6c51fd3417c41a1bc236de6514
Nurkyz-Chynybaeva <nurkyzchynybaeva29@gmail.com>
2021-05-03 15:29:31 +0600

    builder reducer js

--- src/store/builderReducer.js
+++ src/store/builderReducer.js
@@ -1,5 +1,11 @@
 const initialState = {
-
+    buttterflies : {
+        firstButterfly : 5 ,
+        secondButterfly : 5 ,
+        thirdButterfly : 5 ,
+        fourthButterfly : 5 ,
+    },
+  price : 200
 };
 
 const builderReducer = (state = initialState, action) => {
