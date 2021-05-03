b6dcd3f449faf60f4c590b159f8fe737f7d9a501
Nurkyz-Chynybaeva <nurkyzchynybaeva29@gmail.com>
2021-05-03 15:20:56 +0600

    added builder reducer

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import { createStore } from 'redux';
+
+import builderReducer from './store/builderReducer';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+
+    <Provider store = {store}>
+      <BrowserRouter>
     <App />
     </BrowserRouter>
+    </Provider>
+    
   </React.StrictMode>,
   document.getElementById('root')
 );
