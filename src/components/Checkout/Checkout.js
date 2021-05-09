9705ece6fd353707591605a96b03eb2b416bde28
Nurkyz-Chynybaeva <nurkyzchynybaeva29@gmail.com>
2021-05-09 17:29:30 +0600

    export defalut whth axios

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -1,8 +1,9 @@
 import DecorPreview from "../DecorBuilder/DecorPreview/DecorPreview";
 import CheckoutForm from "./CheckoutForm/CheckoutForm";
 import classes from "./Checkout.module.css";
-import axios from "axios";
+import axios from "../../axios";
 import { useSelector } from "react-redux";
+import withAxios from "../withAxios";
 
 const Checkout = ({ history }) => {
   const butterflies = useSelector(state => state.builder.butterflies);
@@ -15,7 +16,7 @@ const Checkout = ({ history }) => {
   function submitCallback(event) {
     const data = new FormData(event.target);
 
-    axios.post('https://builder-c1f06-default-rtdb.firebaseio.com/orders.json', {
+    axios.post('/orders.json', {
       name: data.get('name'),
       address: data.get('address'),
       phone: data.get('phone'),
@@ -31,15 +32,12 @@ const Checkout = ({ history }) => {
   return (
     <div  className={classes.Checkout}>
   
-  < DecorPreview butterflies={butterflies} price={price} />
+  <DecorPreview butterflies={butterflies} price={price} />
       <CheckoutForm
       cancelCallback = {cancelCallback}
       submitCallback = {submitCallback}/>
-
     </div>
   );
 }
- 
-export default Checkout;
-
-
+  
+export default withAxios(Checkout, axios);
