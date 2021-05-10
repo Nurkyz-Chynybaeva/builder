690b13758380fdd24cc314c694cf414c408a3b49
Nurkyz-Chynybaeva <nurkyzchynybaeva29@gmail.com>
2021-05-10 11:12:31 +0600

    fixed order which into the file components

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,5 +1,5 @@
 import axios from "../../axios";
-import { useEffect, useState } from "react";
+import { useEffect} from "react";
 import Order from "./Order/Order";
 import { useDispatch, useSelector } from "react-redux";
 import { load } from "../../store/actions/orders";
