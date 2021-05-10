ea694b85ece223b533d66fe3c67f656728f8e107
Nurkyz-Chynybaeva <nurkyzchynybaeva29@gmail.com>
2021-05-10 10:56:55 +0600

    added backgroud size : cover

--- src/components/DecorBuilder/DecorBuilder.js
+++ src/components/DecorBuilder/DecorBuilder.js
@@ -1,33 +1,36 @@
-import classes from "./DecorBuilder.module.css";
 import DecorPreview from "./DecorPreview/DecorPreview";
 import DecorControls from "./DecorControls/DecorControls";
+
+import classes from "./DecorBuilder.module.css";
 import { useEffect, useState } from "react";
 import axios from "../../axios";
 import Modal from "../Ul/Modal/Modal";
 import OrderSummary from "./OrderSummary/OrderSummary";
 import Button from "../Ul/Button/Button";
-import {useDispatch , useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { load } from "../../store/actions/builder";
 import withAxios from "../withAxios";
 
-const DecorBuilder = ({history}) => {
+const DecorBuilder = ({ history }) => {
   const dispatch = useDispatch();
   const butterflies = useSelector(state => state.builder.butterflies);
   const price = useSelector(state => state.builder.price);
   const [ordering, setOrdering] = useState(false);
 
-
-  // useEffect(loadDefaults, []);
+  useEffect(() => dispatch(load()), []);
 
   // function loadDefaults() {
-  //    axios
-  //     .get('https://builder-c1f06-default-rtdb.firebaseio.com/default.json')
+  //   axios
+  //     .get('https://builder-a51d0-default-rtdb.firebaseio.com/default.json')
   //     .then(response => {
   //       setPrice(response.data.price);
-  //       setButterflies(response.data.butterflies);
-  //     }) 
-  //   }
 
+  //       // For arrays
+  //       // setIngredients(Object.values(response.data.ingredients));
+  //       // For objects
+  //       setIngredients(response.data.ingredients);
+  //     });
+  // }
 
   function startOrdering() {
     setOrdering(true);
@@ -37,33 +40,33 @@ const DecorBuilder = ({history}) => {
     setOrdering(false);
   }
 
-  
   function finishOrdering() {
-        setOrdering(false);
-        // loadDefaults();
-        history.push('/checkout');
-  } ; 
+    setOrdering(false);
+    // loadDefaults();
+    history.push('/checkout');
+  }
 
-  return (<div className={classes.DecorBuilder}>
-    <DecorPreview  
-    butterflies={butterflies} 
-    price={price} />
-    <DecorControls
-      butterflies={butterflies}
-      startOrdering={startOrdering}/>
-     <Modal 
-     show={ordering}
-     cancel={stopOrdering}>
-
-<OrderSummary
+  return (
+    <div className={classes.DecorBuilder}>
+      <DecorPreview
+        butterflies={butterflies}
+        price={price} />
+      <DecorControls
+        butterflies={butterflies}
+        startOrdering={startOrdering}
+        />
+      <Modal
+        show={ordering}
+        cancel={stopOrdering}>
+          <OrderSummary
             butterflies={butterflies}
-            price={price}/>
+            price={price}
+            />
           <Button onClick={finishOrdering} purple>Checkout</Button>
           <Button onClick={stopOrdering}>Cancel</Button>
-     </Modal>
-  </div>
+        </Modal>
+    </div>
   );
 }
 
 export default withAxios(DecorBuilder, axios);
-
