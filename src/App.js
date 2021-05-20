c7b7f9df4f5a808f618f7b4893a9eac1c9e19f6e
Nurkyz-Chynybaeva <nurkyzchynybaeva29@gmail.com>
2021-05-20 22:56:27 +0600

    added auth to the project

--- src/App.js
+++ src/App.js
@@ -4,18 +4,47 @@ import DecorBuilder from "./components/DecorBuilder/DecorBuilder";
 import Checkout from "./components/Checkout/Checkout";
 import Orders from "./components/Orders/Orders";
 import { Redirect, Route, Switch } from "react-router";
+import Auth from "./components/Auth/Auth";
+import Logout from "./components/Logout/Logout";
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
+import { useEffect } from "react";
+import { restore } from "./store/actions/auth";
 
 const App = () => {
+  const dispatch = useDispatch();
+  const isAuthenticated = useSelector(state => state.auth.token !== null);
+
+  useEffect(() => restore(dispatch), [dispatch]);
+
+  let routesOutput = (
+    <Switch>
+      <Route path="/" exact component={DecorBuilder} />
+      <Route path="/auth" component={Auth} />
+      <Redirect to="/" />
+    </Switch>
+  );
+
+
+  if (isAuthenticated) {
+    routesOutput = (
+      <Switch>
+        <Route path="/" exact component={DecorBuilder} />
+        <Route path="/checkout" component={Checkout} />
+        <Route path="/orders" component={Orders} />
+        <Route path="/auth" component={Auth} />
+        <Route path="/logout" component={Logout} />
+        <Redirect to="/" />
+      </Switch>
+    );
+  }
+
+
   return (
     <div className="App">
 
       <Layout>
-        <Switch>
-          <Route path="/" component={DecorBuilder} exact />
-          <Route path="/checkout" component={Checkout} />
-          <Route path="/orders" component={Orders} />
-          <Redirect to="/" />
-        </Switch>
+{routesOutput}
       </Layout>
 
     </div>
--- src/store/actions/types.js
+++ src/store/actions/types.js
@@ -1,4 +1,15 @@
 export const ADD_BUTTERFLY = "ADD_BUTTERFLY";
 export const REMOVE_BUTTERFLY = "REMOVE_BUTTERFLY";
 export const SET_BUTTERFLIES = "SET_BUTTERFLIES";
+
+
 export const SET_ORDERS = "SET_ORDERS";
+
+
+
+
+
+export const AUTH_START = "AUTH_START";
+export const AUTH_SUCCESS = "AUTH_SUCCESS";
+export const AUTH_FAIL = "AUTH_FAIL";
+export const AUTH_LOGOUT = "AUTH_LOGOUT";
